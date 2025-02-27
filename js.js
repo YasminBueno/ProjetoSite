40d610a35d46f84733625d5209aafecba55abc0c
Yasmin Bueno <yasminbbueno@gmail.com>
2025-02-27 14:46:01 -0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,15 @@
+
+
+
+function menu() {
+
+    var menuLateral = document.getElementById("menuLateral");
+
+    if(menuLateral.style.display == "none" || menuLateral.style.display == ""){
+        menuLateral.style.display = "flex"; 
+        
+    }else{
+        menuLateral.style.display = "none"; 
+    }
+    
+}
